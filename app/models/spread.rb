06ae1a5f2c51df0dc24e2d4fb491ddd07860e19b
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-05 16:27:37 -0500

    built rails models

---
+++ app/models/spread.rb
@@ -0,0 +1,3 @@
+class Spread < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20181105212650_create_spreads.rb
@@ -0,0 +1,11 @@
+class CreateSpreads < ActiveRecord::Migration[5.2]
+  def change
+    create_table :spreads do |t|
+      t.string :date
+      t.string :notes
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
