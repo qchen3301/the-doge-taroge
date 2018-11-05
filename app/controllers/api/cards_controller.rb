9ef5d84aa6bac1f412150cbea1a65fd5802a0ed0
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-05 18:36:57 -0500

    cards controller v1

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,2 +1,23 @@
+require 'rubygems'
+require 'httparty'
+
 class Api::CardsController < ApplicationController
+    include HTTParty
+    base_uri 'https://rws-cards-api.herokuapp.com/api/v1/cards'
+
+    def self.draw_two
+        get("/random?n=2")
+    end
+
+    def self.draw_three
+        get("/random?n=3")
+    end
+
+    def self.draw_four
+        get("/random?n=4")
+    end
+
+    def self.draw_five
+        get("/random?n=5")
+    end
 end
