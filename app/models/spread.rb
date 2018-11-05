8a2e68ac7fd36b6497db312b8dc2a2b5ba6a7c14
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-05 16:39:06 -0500

    refactored rails generated models

--- app/models/spread.rb
+++ app/models/spread.rb
@@ -1,3 +1,4 @@
 class Spread < ApplicationRecord
   belongs_to :user
+  has_many :cards, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2018_11_05_212709) do
 
   create_table "cards", force: :cascade do |t|
     t.bigint "spread_id"
+    t.string "card_name"
+    t.string "arcana"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["spread_id"], name: "index_cards_on_spread_id"
