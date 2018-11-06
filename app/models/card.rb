c8f21efd864988a26f9e19b98289ffbde70a3322
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-06 16:22:24 -0500

    cards model refactored

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,13 @@ class Card < ApplicationRecord
   belongs_to :spread
   include HTTParty
   base_uri 'https://rws-cards-api.herokuapp.com/api/v1/cards'
+
+  def self.draw_two(api_id)
+    cards = find_by api_id: api_id
+    return cards unless cards.nil?
+
+    response = get('/random?n=2')
+    create!(  card_name: response['name'],
+              arcana: response['type']  )
+  end
 end
