21069720e466b35e17b586e4b1b263e50f72d132
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-07 15:17:16 -0500

    user.js component making axios calls and rendering properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Users from './components/Users'
 
 export default class App extends Component {
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Users extends Component {
     state = {
-        Users: []
+        users: []
     }
+
+    componentDidMount = async () => {
+        await this.getUsers()
+    }
+
+    getUsers = async() => {
+        const response = await axios.get('/api/users')
+        this.setState({users: response.data})
+    }
+
   render() {
+      const usersContent = this.state.users.map((user, i) => {
+          return(
+              <div key={i}>
+                <h3>{user.name}, user id: {user.id}</h3>
+              </div>
+          )
+      })
     return (
       <div>
-        Here is a list of all users
+        <h1>Hello from Users.js component</h1>
+        <h2>Here is a list of all users</h2>
+        {usersContent}
       </div>
     )
   }
