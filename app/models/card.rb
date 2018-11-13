13acde1c6bed6cc13b76232d55f2966d1de51678
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-12 22:22:53 -0500

    boolean randomizer method working, updated all draw_x methods in Card.rb model to reflect new columns

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,45 +4,45 @@ class Card < ApplicationRecord
   base_uri 'https://rws-cards-api.herokuapp.com/api/v1/cards'
 
   # this method returns either true/false for the 'reversed' column in the Card table
-  def reversed
+  def self.reverseBoolean
     [true,false].sample
   end
 
   def self.draw_two
     response = OpenStruct.new(get('/random?n=2'))
     Card.create!([
-      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"], value: response.cards[0]["value_int"], reversed: self.reversed},
-      {card_name: response.cards[1]["name"], arcana: response.cards[1]["type"], value: response.cards[0]["value_int"], reversed: self.reversed}
+      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"], value: response.cards[0]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[1]["name"], arcana: response.cards[1]["type"], value: response.cards[1]["value_int"], reversed: self.reverseBoolean}
     ])
   end
 
   def self.draw_three
     response = OpenStruct.new(get('/random?n=3'))
     Card.create!([
-      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"]},
-      {card_name: response.cards[1]["name"], arcana: response.cards[1]["type"]},
-      {card_name: response.cards[2]["name"], arcana: response.cards[2]["type"]}
+      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"], value: response.cards[0]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[1]["name"], arcana: response.cards[1]["type"], value: response.cards[1]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[2]["name"], arcana: response.cards[2]["type"], value: response.cards[2]["value_int"], reversed: self.reverseBoolean}
     ])
   end
 
   def self.draw_four
     response = OpenStruct.new(get('/random?n=4'))
     Card.create!([
-      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"]},
-      {card_name: response.cards[1]["name"], arcana: response.cards[0]["type"]},
-      {card_name: response.cards[2]["name"], arcana: response.cards[2]["type"]},
-      {card_name: response.cards[3]["name"], arcana: response.cards[3]["type"]}
+      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"], value: response.cards[0]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[1]["name"], arcana: response.cards[0]["type"], value: response.cards[1]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[2]["name"], arcana: response.cards[2]["type"], value: response.cards[2]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[3]["name"], arcana: response.cards[3]["type"], value: response.cards[3]["value_int"], reversed: self.reverseBoolean}
     ])
   end
 
   def self.draw_five
     response = OpenStruct.new(get('/random?n=5'))
     Card.create!([
-      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"]},
-      {card_name: response.cards[1]["name"], arcana: response.cards[1]["type"]},
-      {card_name: response.cards[2]["name"], arcana: response.cards[2]["type"]},
-      {card_name: response.cards[3]["name"], arcana: response.cards[3]["type"]},
-      {card_name: response.cards[4]["name"], arcana: response.cards[4]["type"]}
+      {card_name: response.cards[0]["name"], arcana: response.cards[0]["type"] value: response.cards[0]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[1]["name"], arcana: response.cards[1]["type"] value: response.cards[1]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[2]["name"], arcana: response.cards[2]["type"] value: response.cards[2]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[3]["name"], arcana: response.cards[3]["type"] value: response.cards[3]["value_int"], reversed: self.reverseBoolean},
+      {card_name: response.cards[4]["name"], arcana: response.cards[4]["type"] value: response.cards[4]["value_int"], reversed: self.reverseBoolean}
     ])
   end
 end
