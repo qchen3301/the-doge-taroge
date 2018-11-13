bc09e00d4da413991f8f054071e4685a3db69a03
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-11-13 11:21:15 -0500

    changing some db files

--- db/migrate/20181105212709_create_cards.rb
+++ db/migrate/20181105212709_create_cards.rb
@@ -1,6 +1,10 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
+      t.string :card_name
+      t.string :arcana
+      t.boolean :reversed
+      t.bigint :value
       t.references :spread, foreign_key: true
       t.timestamps
     end
